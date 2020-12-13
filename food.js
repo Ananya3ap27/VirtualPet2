045d4c6dba73486ca531b4933d29fe9d772fc281
Ananya3ap27 <71383475+Ananya3ap27@users.noreply.github.com>
2020-12-13 19:11:36 +0530

    Update food.js

--- food.js
+++ food.js
@@ -1,10 +1,35 @@
-class Food{
-    constructor(x,y){
-        this.x=x;
-        this.y=y;
-
+class Food {
+    constructor(){
+    this.foodStock=20;
+    this.lastFed;
+    this.image=loadImage('Milk.png');
     }
+
+   updateFoodStock(foodStock){
+    this.foodStock=foodStock;
+   }
+
+   getFedTime(fed){
+     this.fed=fed;
+   }
+
+    getFoodStock(){
+      return this.foodStock;
+    }
+
     display(){
-        image (milkImg,this.x,this.y,10,20);
+      var x=80,y=100;
+
+      imageMode(CENTER);
+      if(this.foodStock!=0){
+        for(var i=0;i<this.foodStock;i++){
+          if(i%10==0){
+            x=80;
+            y+=50;
+          }
+          image(this.image,x,y,50,50);
+          x+=25;
+        }
+      } 
     }
 }
